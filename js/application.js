fd6fb32488249a358046b23f6efad9788bca3645
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-13 14:47:15 +0100

    Give feedback on getUserMedia

--- js/application.js
+++ js/application.js
@@ -1,3 +1,8 @@
 ;(function(){
-    console.log('skeletal application executed');
+    var workbench = document.getElementById('workbench');
+    if (navigator && navigator.getUserMedia) {
+	workbench.innerHTML = 'We are good to go';
+    } else {
+	workbench.innerHTML = 'Something is amiss';
+    }
 })()
