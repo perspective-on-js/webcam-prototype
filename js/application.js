05b6be32828e1160be8cac0652bac81f6e0ca29b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-13 15:16:30 +0100

    Request video

--- js/application.js
+++ js/application.js
@@ -11,6 +11,12 @@
         var video = document.createElement('video');
         video.setAttribute('width', 640);
         workbench.appendChild(video);
+        navigator.getUserMedia({audio: true, video: true}, function(stream) {
+            video.src = window.URL.createObjectURL(stream);
+        }, function(error) {
+            workbench.innerHTML = 'Did not get video';
+            console.log(e);
+        });
     } else {
         workbench.innerHTML = 'Something is amiss';
     }
