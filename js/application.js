2e22e104b6d0bd9b5dea1dcf975ba69535eb486b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-13 14:52:10 +0100

    Monkey path getUserMedia

--- js/application.js
+++ js/application.js
@@ -1,4 +1,11 @@
 ;(function(){
+    if (!navigator.getUserMedia) {
+	navigator.getUserMedia =
+	    navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+	    navigator.msGetUserMedia;
+    }
+
     var workbench = document.getElementById('workbench');
     if (navigator && navigator.getUserMedia) {
 	workbench.innerHTML = 'We are good to go';
